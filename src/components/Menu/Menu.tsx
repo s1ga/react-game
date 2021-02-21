72dc28260dd8f6096f557f1791e2169c5db40410
s1ga <s.v.harlanov@gmail.com>
2021-02-21 17:43:31 +0300

    feat: implement Menu component

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { NavLink, useHistory } from 'react-router-dom'
+import './Menu.css'
+
+interface MenuProps {
+    isOpen: boolean
+    user: string
+    onClose(): void
+    logout(): void
+}
+
+export const Menu: React.FC<MenuProps> = ({ isOpen, user, onClose, logout })=> {
+    const history = useHistory()
+    const classes: String[] = ['Menu']
+
+    const logoutHanlder = (e: React.MouseEvent) => {
+        e.preventDefault()
+        logout()
+        history.push('/')
+    }
+    
+    const clickHandler = (): void => onClose()
+    
+    if (!isOpen) {
+        classes.push('close')
+    }
+
+    return (
+        <>
+            <nav className={classes.join(' ')}>
+                <ul>
+                    <li>
+                        <NavLink activeClassName="active" onClick={clickHandler} to="/home">На главную</NavLink>
+                    </li>
+
+                    <li>
+                        <NavLink activeClassName="active" onClick={clickHandler} to={`/statistics/${user}`}>Статистика</NavLink>
+                    </li>
+
+                    <li>
+                        <a onClick={logoutHanlder} href="/home">На главную</a>
+                    </li>
+                </ul>
+
+                { isOpen ? <Backdrop onClick={onClose} /> : null }
+            </nav>
+        </>
+    )
+}
